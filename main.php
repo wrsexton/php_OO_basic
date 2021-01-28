cd2ad80c94d95c45d5deabab31a96e63d82154d3
reese <reese.sexton01@gmail.com>
2021-01-28 19:19:14 +0000

    Add empty constructor call to adhere to standards.

--- main.php
+++ main.php
@@ -3,8 +3,8 @@
 include_once(dirname(__FILE__) . "/cat.php");
 include_once(dirname(__FILE__) . "/dog.php");
 
-$dog = new Dog;
-$cat = new Cat;
+$dog = new Dog();
+$cat = new Cat();
 $animal_arr = [$dog, $cat, "Test", 1, 2, 3];
 
 foreach ($animal_arr as $animal) {
