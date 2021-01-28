f61b943e3d32cb9b23ae9eb3a5ae5b65af5f57c8
reese <reese.sexton01@gmail.com>
2021-01-28 18:35:10 +0000

    I'm dumb - need to think C, not Python

--- main.php
+++ main.php
@@ -3,8 +3,8 @@
 include_once(dirname(__FILE__) . "/cat.php");
 include_once(dirname(__FILE__) . "/dog.php");
 
-$dog = Dog();
-$cat = Cat();
+$dog = new Dog;
+$cat = new Cat;
 $animal_arr = [$dog, $cat];
 
 foreach ($animal_arr as $animal) {
